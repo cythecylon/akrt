dbdb04de132fb9f8c9d8db543aae01d5a1184d3a
Tim O' Mahony <timomahony91@gmail.com>
2017-03-18 02:59:43 +0000

    '

--- akrt.py
+++ akrt.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 from pathlib import Path
 
 def genwoid():
@@ -17,6 +18,7 @@ def newwo():
         for field in fields:
             f.write(field+": "+input(field+": ")+"\n")
 
+
 def updatewo(woid):
     with open(str(woid)+".txt","at") as f:
         status = input("Please input status as number between 1 and 4.\n\t0)Waiting for bench.\n\t1)In Progress\n\t2)On Hold\n\t3)Ready\n\t4)Checked Out\n\t5)Ambandoned\n:>>")
@@ -25,3 +27,13 @@ def updatewo(woid):
         update = "\nStatus: "+status+"\nNote:\n"+"-"*5+"\n"+note
 
         f.write("\n"+"="*24+update+"\n" + "="*24+"\n")
+
+def main(argv):
+    if argv[0] == 'new':
+        newwo()
+    if argv[0] == 'update':
+        updatewo(argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    
